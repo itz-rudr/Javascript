6c76153cafc6889b7e3f3b1fe3b972cbf8865390
itz-rudr <rudrag26.2@gmail.com>
2026-04-08 23:14:52 +0530

    task

---
+++ ChatGPT/gradesystem.js
@@ -0,0 +1,10 @@
+let marks=850;
+if(marks>90){
+    console.log("A");
+}else if(marks>=75 && marks<=89){
+    console.log("B");
+}else if(marks>=50 && marks<=74){
+    console.log("C");
+}else{
+    console.log("Fail")
+}
