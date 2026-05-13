d8e6ba0dec14f34856d2c8be36099059dea58e71
itz-rudr <rudrag26.2@gmail.com>
2026-05-13 20:36:16 +0530

    understanding of callback stack and queue

--- ChatGPT/callbackFunctions.js
+++ ChatGPT/callbackFunctions.js
@@ -48,4 +48,26 @@ console.log("End");
  * browser and after it has finished execution the browser sends it to callback Queue and then the code is executed 
  * 
  * in SetTimeout function the code runs after a certain amount of time which is fixed by the developer
+ * 
+ * Mini Challenge 1-->
+ * A 
+ * C 
+ * E 
+ * D 
+ * B 
+ * This happens because of the working of javascript is based on stack and queue structure
+ * callback for d enters the Queue first cause it finishes its time first
+ * 
+ * Mini challenge 2-->
+ * output-->
+ * 1
+ * 4
+ * 5
+ * 2
+ * 3
+ * 2-->
+ * 2 callback executes first as it comes first in callback Queue it happens because it goes firstly into the browser and
+ * comes first into the queue
+ * Queue ordering tells about which code block will be executed first
+ * yess timer completion order matters because it tell which execution block is being transferred to callback queue
  */
